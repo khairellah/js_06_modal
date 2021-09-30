cbfaffbdc85f73502922725f71b672644b46ff01
khairellah <60810477+khairellah@users.noreply.github.com>
2021-09-30 11:01:16 +0100

    "add Js code"

--- app.js
+++ app.js
@@ -2,3 +2,20 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+
+/* Get Html Elements */
+const modalBtn          = document.querySelector(".modal-btn");
+const modalOverlayBtn   = document.querySelector(".modal-overlay");
+const closeBtn          = document.querySelector(".close-btn");
+
+/* Add Event */
+modalBtn.addEventListener("click", ()=>{
+/* Add open-modal to the classliste using toggle or 'modalOverlayBtn.classList.add("open-modal")'  */
+modalOverlayBtn.classList.toggle("open-modal")
+})
+
+closeBtn.addEventListener("click", ()=>{
+/* remove open-modal from the classliste using toggle or 'modalOverlayBtn.classList.remove("open-modal")'  */
+modalOverlayBtn.classList.toggle("open-modal")    
+    })
